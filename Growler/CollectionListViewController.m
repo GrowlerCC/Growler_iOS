e5d82bf7fff741cd8192b4c05483be509b100cc0
Alexander Gorbovets <gorsash@gmail.com>
2017-02-19 14:18:08 +0300

    Fixed button order

--- Growler/CollectionListViewController.m
+++ Growler/CollectionListViewController.m
@@ -75,7 +75,7 @@
     UIImage *checkoutButtonImage = [[UIImage imageNamed: @"CheckoutButton"] imageWithRenderingMode: UIImageRenderingModeAlwaysOriginal];
     id checkoutButton = [[UIBarButtonItem alloc] initWithImage: checkoutButtonImage style: UIBarButtonItemStylePlain target: self action: @selector(didTapMenuButton)];
 
-    self.navigationController.topViewController.navigationItem.rightBarButtonItems = @[searchButton, checkoutButton];
+    self.navigationController.topViewController.navigationItem.rightBarButtonItems = @[checkoutButton, searchButton];
 }
 
 - (void)didTapMenuButton {
