c6e0a08682d429885fffab9544d1adb50141f1c4
Alexander Gorbovets <gorsash@gmail.com>
2017-02-23 16:27:43 +0300

    Added collection identifiers

---
+++ Growler/Components/CollectionIdentifier.swift
@@ -0,0 +1,24 @@
+//
+// Created by Alexander Gorbovets on 2017-02-23.
+// Copyright (c) 2017 growler. All rights reserved.
+//
+
+import Foundation
+
+enum CollectionIdentifier: Int64 {
+
+    case freshBrews = 414175565
+
+    case mostPopular = 403523213
+
+    case breakfastOfChampions = 408896397
+
+    case gameDay = 403519053
+
+    case cheeseLovers = 403516621
+
+    case staffPicks = 403516557
+
+    case ciceronesChoiceBoozyGueuze = 401856909
+
+}
