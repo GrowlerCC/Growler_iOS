60546b26d90908b798d0cf5b66bf2916dc6dc867
Alexander Gorbovets <gorsash@gmail.com>
2017-02-23 13:43:59 +0300

    Added ActivityIndicatorTableCell

---
+++ Growler/Table View Cell/ActivityIndicatorTableCell.swift
@@ -0,0 +1,8 @@
+//
+// Created by Alexander Gorbovets on 2017-02-23.
+// Copyright (c) 2017 growler. All rights reserved.
+//
+
+import Foundation
+
+class ActivityIndicatorTableCell: UITableViewCell {}
