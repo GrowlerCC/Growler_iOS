53c685b53dd656085cf0ffb341e89f13ff9cbc07
Alexander Gorbovets <gorsash@gmail.com>
2017-02-24 21:01:39 +0300

    Implemented recommendations list

--- Growler/View Controllers/RecommendationListViewController.swift
+++ Growler/View Controllers/RecommendationListViewController.swift
@@ -5,5 +5,28 @@
 
 import Foundation
 
-class RecommendationListViewController: AbstractProductListViewController {
+class RecommendationListViewController: ProductListViewController {
+
+    private var footer: UIView!
+
+    private var checkoutButton: UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Recommendations"
+    }
+
+    override func loadProducts() {
+        // important: don't call super! we don't want to show all products, but only ones in the cart
+        _ = getProductsPage(page: 1).then {
+            (products) -> Void in
+            self.products = ShopifyController.selectRecommendedProducts(from: products)
+            mq { self.tableView.reloadData() }
+        }
+    }
+
+    func checkout() {
+        ShopifyController.instance.checkout(navigationController: navigationController!)
+    }
+
 }
