3d99103ab999e4dc9acb28f95778fcad89661bcc
Alexander Gorbovets <gorsash@gmail.com>
2017-02-24 23:20:49 +0300

    Added extension for notifications

---
+++ Growler/Components/Notifications.swift
@@ -0,0 +1,35 @@
+//
+// Created by Alexander Gorbovets on 2017-02-24.
+// Copyright (c) 2017 growler. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+
+    static let favoritesChanged = Notification.Name("favoritesChanged")
+    
+    static let cartChanged = Notification.Name("cartChanged")
+
+    func send() {
+        NotificationCenter.default.post(name: self, object: nil)
+    }
+    
+}
+
+extension UIViewController {
+
+    func subscribeTo(notification: Notification.Name, selector: Selector) {
+        NotificationCenter.default.addObserver(
+                self,
+                selector: selector,
+                name: notification,
+                object: nil
+            )
+    }
+
+    func unsubscribeFromNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+}
