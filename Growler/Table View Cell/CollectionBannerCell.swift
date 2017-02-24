eeaf850550b83b73cff0c4fe467981fbb8347e69
Alexander Gorbovets <gorsash@gmail.com>
2017-02-24 17:05:10 +0300

    Implemented viewing categories when tapping on their banners

--- Growler/Table View Cell/CollectionBannerCell.swift
+++ Growler/Table View Cell/CollectionBannerCell.swift
@@ -13,14 +13,22 @@ class CollectionBannerCell: UITableViewCell {
     
     @IBOutlet weak var descriptionLabel: UILabel!
 
+    private var recognizer: UITapGestureRecognizer!
+
+    private var collectionId: CollectionIdentifier!
+
+    private var collection: BUYCollection!
+
     class func create(collectionId: CollectionIdentifier) -> CollectionBannerCell {
         let cell = CollectionBannerCell.loadFromNib()
+        cell.collectionId = collectionId
 
         ShopifyController.instance.client.getCollectionsByIds([String(collectionId.rawValue)], page: 1) {
             collections, page in
             guard let collection = collections?.first else {
                 return
             }
+            cell.collection = collection
             if let url = collection.image?.sourceURL.absoluteURL {
                 cell.picture.loadImage(with: url, completion: nil)
             } else {
@@ -31,6 +39,20 @@ class CollectionBannerCell: UITableViewCell {
         }
 
         return cell
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        recognizer = UITapGestureRecognizer(target: self, action: #selector(self.didTap(_:)))
+        addGestureRecognizer(recognizer)
+    }
+
+    func didTap(_ sender: UITapGestureRecognizer) {
+        guard collection != nil else {
+            return
+        }
+        let controller = ProductListViewController(client: ShopifyController.instance.client, collection: collection)!
+        AppDelegate.shared.navigationController.pushViewController(controller, animated: true)
     }
 
 }
