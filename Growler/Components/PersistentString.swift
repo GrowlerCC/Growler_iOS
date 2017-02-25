78a07c4671e88e4e0e004fa344bd4e1b3dde71b4
Alexander Gorbovets <gorsash@gmail.com>
2017-02-25 05:46:44 +0300

    Implemented persistent string

---
+++ Growler/Components/PersistentString.swift
@@ -0,0 +1,29 @@
+//
+// Created by Alexander Gorbovets on 2017-02-25.
+// Copyright (c) 2017 growler. All rights reserved.
+//
+
+import Foundation
+
+class PersistentString {
+
+    let defaultsKey: String
+
+    let changeNotification: Notification.Name
+
+    init(defaultsKey: String, changeNotification: Notification.Name) {
+        self.defaultsKey = defaultsKey
+        self.changeNotification = changeNotification
+    }
+
+    var value: String {
+        get {
+            return UserDefaults.standard.string(forKey: defaultsKey) ?? ""
+        }
+
+        set(newValue) {
+            UserDefaults.standard.set(newValue, forKey: defaultsKey)
+        }
+    }
+
+}
